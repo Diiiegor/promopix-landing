807da8691ec16d3336348b30453ceb5fc2b3286a
Juan Diego <Juandiegooliverosrios.d@gmail.com>
2023-06-18 16:14:16 -0500

    Early access form

--- src/js/components/EarlyAccess.tsx
+++ src/js/components/EarlyAccess.tsx
@@ -1,11 +1,8 @@
 const EarlyAccess = () => {
   return (
     <div className="early-access">
-      <h2>Early Access</h2>
-      <form>
-        <input type="text" placeholder="Type your email"/>
-        <button>Subscribe</button>
-      </form>
+        <input type="text" placeholder="Enter your email here"/>
+        <button>GET EARLY ACCESS</button>
     </div>
   );
 }
